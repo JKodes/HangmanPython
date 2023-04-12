f9344a6d6414d36e530ccdcacb2defd83e96e0ad
JKodes <adrainford09@gmail.com>
2023-04-12 18:11:32 -0400

    create a list of words for hangman game

---
+++ Hangman.py
@@ -0,0 +1,6 @@
+
+
+
+python_fundamentals = ['strings', 'integers', 'float', 'Boolean', 'arrays', 'dictionary', 'set', 'tuples', 
+                       'function', 'while loop', 'for loop', 'if statment']
+
