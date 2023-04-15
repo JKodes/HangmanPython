95ded3ccc95ad99ee6d04342ce39ca677779474c
DevJay <jawanistanford@DevJays-MacBook-Pro.local>
2023-04-15 00:23:11 -0400

    import the random module to use choicemethod this will pick a random letter from the list and then have a for loop that looks for loop that iterates through the word that was picked the if statement would predict if the word was correct by display right or wrong for the supposed to be position of the letter for the word

--- Hangman.py
+++ Hangman.py
@@ -4,3 +4,16 @@
 python_fundamentals = ['strings', 'integers', 'float', 'Boolean', 'arrays', 'dictionary', 'set', 'tuples', 
                        'function', 'while loop', 'for loop', 'if statment']
 
+import random
+
+chosen_word = random.choice(python_fundamentals)
+
+
+
+guess = input("Guess a letter: ").lower()
+
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
