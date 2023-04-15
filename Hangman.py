d5730c1474b34d5cedab70bfde9239f2e78f8cb9
JKodes <adrainford09@gmail.com>
2023-04-15 13:14:14 -0400

    place the for loop inside of a while loop

--- Hangman.py
+++ Hangman.py
@@ -14,14 +14,21 @@ for _ in range(word_length):
     display += "_"
 print(display)
 
+end_of_game = False
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    print(display)
 
 
-guess = input("Guess a letter: ").lower()
+    if "_" not in display:
+        end_of_game == True
+        print("You when win")
 
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
-
-print(display)
     
