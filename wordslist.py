e01fb3cacfb157fff2cec8be6fdcbf05d6ee2019
JKodes <adrainford09@gmail.com>
2023-04-25 23:59:55 -0400

    create a seperate wordlist file for the game

---
+++ wordslist.py
@@ -0,0 +1,22 @@
+python_fundamentals = [
+'strings', 
+'integers',
+'float', 
+'Boolean', 
+'arrays', 
+'dictionary',
+'variable', 
+'set', 
+'zen of python',
+'tuples',
+'return', 
+'function',
+'while loop',
+'for loop', 
+'if statment'
+'class',
+'init',
+'module',
+'lamba',,
+'pythonic'
+]
