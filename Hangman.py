bb68572aed17393c26af01d81fb42a2a958c5336
JKodes <adrainford09@gmail.com>
2023-04-26 22:50:03 -0400

    finish python hangman game is complete

--- Hangman.py
+++ Hangman.py
@@ -1,23 +1,19 @@
-
-
-
-import wordslist
-
 import random
 
+import wordslist
 chosen_word = random.choice(wordslist.python_fundamentals)
+word_length = len(chosen_word)
+
+end_of_game = False
 lives = 6
 
 from hangmanart import logo, stages
 print(logo)
 
 display = []
-word_length = len(chosen_word)
 for _ in range(word_length):
     display += "_"
-print(display)
-
-end_of_game = False
+   
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
@@ -30,17 +26,20 @@ while not end_of_game:
         if letter == guess:
             display[position] = letter
 
-if guess not in chosen_word:
-    lives -= 1
-    if lives == 0:
-        end_of_game = True
-        print("You lose")
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. For that a life is loss")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose")
 
-    if "_" not in display:
-        end_of_game == True
-        print("You win")
+        print(f"{' '.join(display)}")
+        
+        if "_" not in display:
+            end_of_game == True
+            print("You win")
 
-    import hangmanart
-    print(stages[lives])
+        
+        print(stages[lives])
 
     
